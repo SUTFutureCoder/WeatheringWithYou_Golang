97970b6bfce34606c074f6f9b2e3a8c314068de4
xingchen.lin <xingchen.lin@ele.me>
2019-11-17 17:22:40 +0800

    add slice controll

--- actions/analyse.go
+++ actions/analyse.go
@@ -13,6 +13,8 @@ type Analyse struct {
 	SwLat float64 `json:"sw_lat"`
 	NeLng float64 `json:"ne_lng"`
 	NeLat float64 `json:"ne_lat"`
+	WSlice float64 `json:"wslice"`
+	HSlice float64 `json:"hslice"`
 }
 
 func (o *Analyse) AnalysePoint() (func(ctx *gin.Context)) {
@@ -23,15 +25,18 @@ func (o *Analyse) AnalysePoint() (func(ctx *gin.Context)) {
 		if err != nil {
 			fmt.Println(err)
 		}
-
+fmt.Println(reqInfo)
 		// STEP2 计算每块数据量
 		// 12:9对区块进行切割 假定前端最多处理5w点，每个各自返回500随机点数据
 		var ch = make(chan []model.Point, 1000)
 
 		// STEP3 请求opensearch获取
 		// 对区块进行分割，进行随机处理
-		lngSlice := (reqInfo.NeLng - reqInfo.SwLng) / 20
-		latSlice := (reqInfo.NeLat - reqInfo.SwLat) / 10
+		lngSlice := (reqInfo.NeLng - reqInfo.SwLng) / reqInfo.WSlice
+		latSlice := (reqInfo.NeLat - reqInfo.SwLat) / reqInfo.HSlice
+
+		fmt.Println(lngSlice)
+		fmt.Println(latSlice)
 
 		waitGroup := &sync.WaitGroup{}
 		for currLng := reqInfo.SwLng; currLng < reqInfo.NeLng; currLng += lngSlice {
@@ -40,6 +45,7 @@ func (o *Analyse) AnalysePoint() (func(ctx *gin.Context)) {
 				go model.AnalysePoints(waitGroup, ch, currLng, currLat, currLng + lngSlice, currLat + latSlice, 500,1)
 			}
 		}
+		fmt.Println(waitGroup)
 		waitGroup.Wait()
 		close(ch)
 
