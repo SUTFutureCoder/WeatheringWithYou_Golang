f17a8c4540d96a6d3d238bf2375035bf2a5140d8
xingchen.lin <xingchen.lin@ele.me>
2019-11-28 23:10:03 +0800

    update

--- main.go
+++ main.go
@@ -23,6 +23,6 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	//r.Run(":17565") // あまの ひな
-	r.Run(":3001") // あまの ひな
+	r.Run(":17565") // あまの ひな
+	//r.Run(":3001")
 }
